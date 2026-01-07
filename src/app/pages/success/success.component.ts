2e7632f7f0311fff2f2f36cd9a972f3250dce365
12312839 <e12312839@student.tuwien.ac.at>
2026-01-07 06:14:37 +0100

    Success seite erstellt, style, footer, header überarbeitet

--- src/app/pages/success/success.component.ts
+++ src/app/pages/success/success.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-success',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './success.component.html',
   styleUrl: './success.component.css',
 })
