453643108e03a6b0fe01f08c71a8b03a5e05a979
12312839 <e12312839@student.tuwien.ac.at>
2026-01-07 07:38:55 +0100

    missing import

--- src/app/pages/pricing/pricing.component.ts
+++ src/app/pages/pricing/pricing.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-pricing',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.css',
 })
