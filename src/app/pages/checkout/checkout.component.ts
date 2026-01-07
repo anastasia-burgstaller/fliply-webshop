2b2f4d1f39ba61fde2f80ed12e4263244451890e
12312839 <e12312839@student.tuwien.ac.at>
2026-01-07 06:10:54 +0100

    Checkout erstellt

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css',
 })
